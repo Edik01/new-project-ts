adc4845bf1c1e6fbe98ddea3e73a7d797373f253
Edik Huloian <guloyan.edik@gmail.com>
2024-01-04 21:33:03 -0800

    summary

--- nodemail.js
+++ nodemail.js
@@ -1,1 +1,25 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
+const config = {
+  host: "smt.meta.ua",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "goitnodejs@meta.ua",
+    pass: process.env.PASSWORD,
+  },
+};
+
+const transporter = nodemailer.createTransport(config);
+const emailOptions = {
+  from: "goitnodejs@meta.ua",
+  to: "noresponse@gmail.com",
+  subject: "Nodemailer test",
+  text: "Привіт. Ми тестуємо надсилання листів!",
+};
+
+transporter
+  .sendMail(emailOptions)
+  .then((info) => console.log(info))
+  .catch((err) => console.log(err));
